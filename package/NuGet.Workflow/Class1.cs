9d458fad10345adbdf10926084ea62d0dd93222d
jtownsend-technolog-ltd <jtownsend@technolog.com>
2024-02-21 15:19:24 +0000

    Update to test ci.yml Action.

--- package/NuGet.Workflow/Class1.cs
+++ package/NuGet.Workflow/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace NuGet.Workflow
+namespace NuGet.Workflow
 {
    public class Class1
    {
+      /// <summary>
+      /// Value
+      ///
+      /// Added comment for GitHub Test!!!
+      /// </summary>
       public string Value { get; set; } = "Foo";
    }
 }
